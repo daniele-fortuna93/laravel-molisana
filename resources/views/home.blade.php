1e0fa823d9616824a4a3eb3d85f3e4636ec05545
Daniele <bambudio@libero.it>
2021-05-07 16:04:54 +0200

    Inizio progetto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>La Molisana</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        @include('partials/header')
+        <main>
+            <div class="container">
+                <section>
+                    <h2>Le Lunghe</h2>
+                    <div class="cards">
+                        @foreach ($lunghe as $pasta)
+                            <div class="card">
+                                <img src="{{ $pasta['src'] }}" alt="{{ $pasta['titolo'] }}">
+                            </div>
+                        @endforeach
+                    </div>
+                </section>
+            </div>
+        </main>
+        <footer></footer>
+    </body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="logo">
+        <img src="{{ asset('images/marchio-sito-test.png') }}" alt="Logo La Molisana">
+    </div>
+    <nav>
+        <ul>
+            <li><a class="active" href="#">Home</a></li>
+            <li><a href="#">Prodotti</a></li>
+            <li><a href="#">News</a></li>
+        </ul>
+    </nav>
+</header>
